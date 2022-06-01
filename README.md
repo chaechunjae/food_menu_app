5cba3468f5ffc21723ef1ac750a01fe371496ed1
MyengJu-Chae <myeongju@dable.io>
2022-06-01 18:59:42 +0900

    add user profile model

---
+++ README.md
@@ -0,0 +1,7 @@
+### FOOD MENU WEB APPLICATION
+---
+dependency
+```
+poetry 0.1.0
+$ poetry install
+```
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import RegisterForm
 
@@ -10,9 +11,13 @@ def register(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}, your account is created')
-            return redirect('food:index')
+            return redirect('login')
     else:
         form = RegisterForm()
 
     return render (request, 'users/register.html', {'form':form})
+
+@login_required
+def profilepage(request):
+    return render(request, 'users/profile.html')
 
