40811dc42a9624cf5318e28338e57f55abdb185f
MyengJu-Chae <myeongju@dable.io>
2022-06-03 00:29:44 +0900

    adding the user profile picture

--- food_menu_app/urls.py
+++ food_menu_app/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as authentication_view
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,7 @@ urlpatterns = [
     path('logout/', authentication_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profilepage, name='profile'),
 ]
+
+urlpatterns = [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
