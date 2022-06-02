187fc1866a51599974b33d82f1e28eb2ef7593e2
MyengJu-Chae <myeongju@dable.io>
2022-06-03 02:00:42 +0900

    edit user register

--- users/views.py
+++ users/views.py
@@ -9,6 +9,7 @@ def register(request):
     if request.method == 'POST':
         form =  RegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}, your account is created')
             return redirect('login')
