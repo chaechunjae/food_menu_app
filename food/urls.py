e3588710f6bebb1122271c4e58c218e7eb1fcb1e
MyengJu-Chae <myeongju@dable.io>
2022-06-05 02:15:24 +0900

    Add class based view for detail

--- food/urls.py
+++ food/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.IndexClassView.as_view(), name='index'),
     # /food/1
-    path('<int:item_id>', views.detail, name='detail'),
+    # path('<int:item_id>', views.detail, name='detail'),
+    path('<int:pk>/', views.FoodDetail.as_view(), name='detail'),
     path('item/', views.items, name='item'),
     # add items
     path('add', views.create_item, name='create_item'),
